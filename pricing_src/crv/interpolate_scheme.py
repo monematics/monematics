d44e3801ca6a772077672f4e4f590bb974c3f63e
leo zhang <leo.zhang927@gmail.com>
2025-02-22 22:33:01 -0500

    pushing wip

--- pricing_src/crv/interpolate_scheme.py
+++ pricing_src/crv/interpolate_scheme.py
@@ -4,3 +4,16 @@ Interpolation scheme used in pricing
 2. APQ2N and 4N
 3. Cubic Spline
 """
+from abc import ABC
+
+
+class InterpolationScheme(ABC):
+    pass
+
+
+class ExtrapolationScheme:
+    def __init__(self, x):
+        self.x = x
+
+    def __call__(self, *args, **kwargs):
+        return self.x
